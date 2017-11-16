cecb230ae7ea862af3dda17985d09e50288c4214
Chotika Luangorachorn <chotika.lu@ku.th>
2017-11-17 00:59:32 +0700

    create customer & manager fxml

---
+++ API/src/main/java/Destination.java
@@ -0,0 +1,32 @@
+public class Destination {
+    private String province;
+    private String district;
+    private String place;
+    private Double distance;
+
+    public Destination(String province, String district, String place) {
+        this.province = province;
+        this.district = district;
+        this.place = place;
+    }
+
+    public void setDistance(Double distance) {
+        this.distance = distance;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public Double getDistance() {
+        return distance;
+    }
+}
---
+++ API/src/main/java/Van.java
@@ -0,0 +1,23 @@
+public class Van {
+    private String regisNumber;
+    private String partnerId;
+    private String type;
+
+    public Van(String regisNumber, String partnerId, String type) {
+        this.regisNumber = regisNumber;
+        this.partnerId = partnerId;
+        this.type = type;
+    }
+
+    public String getRegisNumber() {
+        return regisNumber;
+    }
+
+    public String getPartnerId() {
+        return partnerId;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
