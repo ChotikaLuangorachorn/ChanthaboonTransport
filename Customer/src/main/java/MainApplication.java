436068c9b62d7f12dd97ae2076fedafe3a06f54c
WEELT5602DZO721\Phornrawin <phornrawin.c@ku.th>
2017-11-17 14:52:23 +0700

    extends Application

--- Customer/src/main/java/MainApplication.java
+++ Customer/src/main/java/MainApplication.java
@@ -1,13 +1,14 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Main extends Application{
+public class MainApplication extends Application {
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    public void start(Stage primaryStage) throws Exception {
+    @Override
+    public void start(Stage primaryStage) {
 
     }
 }
