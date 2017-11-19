c34b88994af00b8b340a4ed3b1903918a470311d
natthapach <natthapach.a@ku.th>
2017-11-19 19:52:27 +0700

    API: add DateFormatter

---
+++ API/src/main/java/utils/ReservationDateFormatter.java
@@ -0,0 +1,20 @@
+package utils;
+
+import models.Reservation;
+
+import java.text.SimpleDateFormat;
+
+public class ReservationDateFormatter {
+    private static ReservationDateFormatter instance;
+    private static SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+    public static ReservationDateFormatter getInstance(){
+        if (instance == null)
+            instance = new ReservationDateFormatter();
+        return instance;
+    }
+
+    public static SimpleDateFormat getFormatter() {
+        return formatter;
+    }
+}
