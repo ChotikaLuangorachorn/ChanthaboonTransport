9d0f82355363ec3726052dd7633d1ef0f298922b
natthapach <natthapach.a@ku.th>
2017-11-20 15:26:58 +0700

    API: create 3 formatter

--- API/src/main/java/utils/ReservationDateFormatter.java
+++ API/src/main/java/utils/ReservationDateFormatter.java
@@ -7,15 +7,24 @@ import java.util.Locale;
 
 public class ReservationDateFormatter {
     private static ReservationDateFormatter instance;
-    private static SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.ENGLISH);
-
+    private SimpleDateFormat dbFormatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.ENGLISH);
+    private SimpleDateFormat uiDateFormatter = new SimpleDateFormat("dd/MM/yyyy");
+    private SimpleDateFormat uiTimeFormatter = new SimpleDateFormat("hh:mm:ss");
     public static ReservationDateFormatter getInstance(){
         if (instance == null)
             instance = new ReservationDateFormatter();
         return instance;
     }
 
-    public SimpleDateFormat getFormatter() {
-        return formatter;
+    public SimpleDateFormat getDbFormatter() {
+        return dbFormatter;
+    }
+
+    public SimpleDateFormat getUiDateFormatter() {
+        return uiDateFormatter;
+    }
+
+    public SimpleDateFormat getUiTimeFormatter() {
+        return uiTimeFormatter;
     }
 }
