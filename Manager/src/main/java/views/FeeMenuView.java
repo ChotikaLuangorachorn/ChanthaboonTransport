f5ef715500068865d8e09d67b215129c6a63a470
Chotika Luangorachorn <chotika.lu@ku.th>
2017-11-23 20:38:38 +0700

    Manager:fee_menu.fxml- delete Button

--- Manager/src/main/java/views/FeeMenuView.java
+++ Manager/src/main/java/views/FeeMenuView.java
@@ -4,17 +4,24 @@ import controllers.MainController;
 import controllers.StageController;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 
-public class FeeMenuView {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FeeMenuView implements Initializable{
+    @FXML private TableView table_fee;
+    @FXML private TableColumn col_vanType, col_reserveType, col_rate, col_base;
+    @FXML private Button btn_editFee;
     @FXML private Label lbCometoMain;
     private MainController controller;
     private StageController stageController;
 
-    public void setController(MainController controller) {
-        this.controller = controller;
-    }
 
     public void setStageController(StageController stageController) {
         this.stageController = stageController;
@@ -24,6 +31,11 @@ public class FeeMenuView {
                 stageController.showMainView();
             }
         });
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
     }
+
 }
