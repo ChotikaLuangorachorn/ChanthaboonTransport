a67ae68362d42f9bdc8a4fba5fc128890ba92bd2
WEELT5602DZO721\Phornrawin <phornrawin.c@ku.th>
2017-12-09 17:14:28 +0700

    Manager: implement ReservationEditView complete

--- Manager/src/main/java/views/ReservationEditView.java
+++ Manager/src/main/java/views/ReservationEditView.java
@@ -1,15 +1,20 @@
 package views;
 
 import controllers.MainController;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
 import models.Reservation;
+import utils.ReservationDateFormatter;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class ReservationEditView implements Initializable {
@@ -25,11 +30,39 @@ public class ReservationEditView implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        onClickBtnConfirm();
+    }
 
+    public void setDataReservation(){
+        lbReservationId.setText(reservation.getReserveId());
+        lbDate.setText(ReservationDateFormatter.getInstance().getUiDateFormatter().format(reservation.getStartDate()));
+        lbPayDate.setText(ReservationDateFormatter.getInstance().getUiDateFormatter().format(reservation.getDepositDate()));
+        lbStatus.setText(reservation.getIsDeposited());
+        spnHrs.getValueFactory().setValue(reservation.getStartDate().getHours());
+        spnMin.getValueFactory().setValue(reservation.getStartDate().getMinutes());
+        taPlace.setText(reservation.getMeetingPlace());
+
+    }
+    public void onClickBtnConfirm(){
+        btnConfirm.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                int hr = (int) spnHrs.getValueFactory().getValue();
+                int mi = (int) spnMin.getValueFactory().getValue();
+                reservation.getStartDate().setHours(hr);
+                reservation.getStartDate().setMinutes(mi);
+                controller.addMeeting(taPlace.getText(), reservation.getStartDate(), reservation);
+
+                Stage stage = (Stage) taPlace.getScene().getWindow();
+                stage.close();
+
+            }
+        });
     }
 
     public void setReservation(Reservation reservation) {
         this.reservation = reservation;
+        setDataReservation();
     }
 
     public void setController(MainController controller) {
