8b5e0f5c6da27eed8968f48d8056f3efd5c5a734
natthapach <natthapach.a@ku.th>
2017-12-09 12:22:17 +0700

    Server: delegating SQLExecutor

--- Server/src/main/java/controllers/SQLiteExecutor.java
+++ Server/src/main/java/controllers/SQLiteExecutor.java
@@ -20,10 +20,11 @@ public class SQLiteExecutor implements CustomerDatabaseManager, ManagerDatabaseM
     private String url = "vanScheduler.db";
     private SimpleDateFormat formatter = ReservationDateFormatter.getInstance().getDbFormatter();
     private VanExecutor vanExecutor;
-    private final DriverExecutor driverExecutor;
-    private final CustomerExecutor customerExecutor;
-    private final PartnerExecutor partnerExecutor;
-    private final ReservationExecutor reservationExecutor;
+    private PriceExecutor priceExecutor;
+    private DriverExecutor driverExecutor;
+    private CustomerExecutor customerExecutor;
+    private PartnerExecutor partnerExecutor;
+    private ReservationExecutor reservationExecutor;
 
     public SQLiteExecutor() {
         vanExecutor = new VanExecutor(url);
@@ -31,6 +32,7 @@ public class SQLiteExecutor implements CustomerDatabaseManager, ManagerDatabaseM
         customerExecutor = new CustomerExecutor(url);
         partnerExecutor = new PartnerExecutor(url);
         reservationExecutor = new ReservationExecutor(url);
+        priceExecutor = new PriceExecutor(url);
     }
 
     @Nullable
@@ -43,84 +45,15 @@ public class SQLiteExecutor implements CustomerDatabaseManager, ManagerDatabaseM
     }
 
     public double getPrice(Map<String, Integer> vanAmt, Date startDate, Date endDate) {
-        System.out.println("request getPrice");
-        System.out.println("params " + vanAmt + " startDate " + startDate + " endDate " + endDate);
-
-        int vipAmt = vanAmt.get(CustomerDatabaseManager.VIP);
-        int normalAmt = vanAmt.get(CustomerDatabaseManager.NORMAL);
-        long diff = endDate.getTime() - startDate.getTime();
-        int days = (int) diff / (24*60*60*1000);
-        String sql = "select * from price_rate where price_rate.reserve_type = \"day\"";
-
-        QueryExecutionAssistant<Double> assistant = new QueryExecutionAssistant<>(url);
-        return assistant.execute(sql, (resultSet -> {
-                    Map<String, Double> rate = new HashMap<String, Double>();
-                    Map<String, Double> base = new HashMap<String, Double>();
-                    Map<String, Double> freeRage = new HashMap<String, Double>();
-
-                    while (resultSet.next()){
-                        rate.put(resultSet.getString("van_type"), resultSet.getDouble("rate"));
-                        base.put(resultSet.getString("van_type"), resultSet.getDouble("base"));
-                        freeRage.put(resultSet.getString("van_type"), resultSet.getDouble("free_range"));
-                    }
-
-                    double normalPrice = base.get(CustomerDatabaseManager.NORMAL) + rate.get(CustomerDatabaseManager.NORMAL)*((days < freeRage.get(CustomerDatabaseManager.NORMAL))?0:(days-freeRage.get(CustomerDatabaseManager.NORMAL)));
-                    double vipPrice = base.get(CustomerDatabaseManager.VIP) + rate.get(CustomerDatabaseManager.VIP)*((days < freeRage.get(CustomerDatabaseManager.VIP))?0:(days-freeRage.get(CustomerDatabaseManager.VIP)));
-
-                    return normalPrice*normalAmt + vipPrice*vipAmt;
-                }), 0.0);
+        return priceExecutor.getPrice(vanAmt, startDate, endDate);
     }
 
     public double getPrice(Map<String, Integer> vanAmt, Destination destination) {
-        System.out.println("request getPrice");
-        System.out.println("params " + vanAmt + " destination " + destination);
-        int vipAmt = vanAmt.get(CustomerDatabaseManager.VIP);
-        int normalAmt = vanAmt.get(CustomerDatabaseManager.NORMAL);
-        String sql = "select * from price_rate where price_rate.reserve_type = \"distance\"";
-
-        QueryExecutionAssistant<Double> assistant = new QueryExecutionAssistant<>(sql);
-        return assistant.execute(sql, (resultSet -> {
-            double distance = getDistance(destination);
-            Map<String, Double> rate = new HashMap<String, Double>();
-            Map<String, Double> base = new HashMap<String, Double>();
-            Map<String, Double> freeRage = new HashMap<String, Double>();
-            while (resultSet.next()){
-                rate.put(resultSet.getString("van_type"), resultSet.getDouble("rate"));
-                base.put(resultSet.getString("van_type"), resultSet.getDouble("base"));
-                freeRage.put(resultSet.getString("van_type"), resultSet.getDouble("free_range"));
-            }
-
-            double normalPrice = base.get(CustomerDatabaseManager.NORMAL) + rate.get(CustomerDatabaseManager.NORMAL)*((distance < freeRage.get(CustomerDatabaseManager.NORMAL))?0:(distance-freeRage.get(CustomerDatabaseManager.NORMAL)));
-            double vipPrice = base.get(CustomerDatabaseManager.VIP) + rate.get(CustomerDatabaseManager.VIP)*((distance < freeRage.get(CustomerDatabaseManager.VIP))?0:(distance-freeRage.get(CustomerDatabaseManager.VIP)));
-            System.out.println("base = " + base);
-            System.out.println("rate = " + rate);
-            System.out.println("freeRage = " + freeRage);
-            System.out.println("normalPrice = " + normalPrice);
-            System.out.println("vipPrice = " + vipPrice);
-            return normalPrice*normalAmt + vipPrice*vipAmt;
-        }), null);
-
+        return priceExecutor.getPrice(vanAmt, destination);
     }
 
     public void addReservation(String customerId, Map<String, Integer> vanAmt, Destination destination, Date startDate, Date endDate, Date reserveDate, double price, double deposit) {
-        System.out.println("request addReservation");
-        Connection connection = null;
-        try{
-            String reserveDateString = formatter.format(reserveDate);
-            String startDateString = formatter.format(startDate);
-            String endDateString = formatter.format(endDate);
-
-            connection = prepareConnection();
-            if (connection != null) {
-                String sql = String.format("insert into reservation (customer_id, reserve_date, start_working_date, end_working_date, fee, province, district, place, isDeposit, isDeposited, deposit_fee, amt_vip, amt_normal) values ('%s', '%s', '%s', '%s', %f, '%s', '%s', '%s', '%s', '%s', %f, %d, %d)",
-                                            customerId, reserveDateString, startDateString, endDateString, price, destination.getProvince(), destination.getDistrict(), destination.getPlace(), "true", "false", deposit, vanAmt.get(CustomerDatabaseManager.VIP), vanAmt.get(CustomerDatabaseManager.NORMAL));
-                Statement statement = connection.createStatement();
-                int result = statement.executeUpdate(sql);
-                System.out.println("result = " + result);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        reservationExecutor.addReservation(customerId, vanAmt, destination, startDate, endDate, reserveDate, price, deposit);
     }
 
     public Van getVan(String vanId){
@@ -754,39 +687,9 @@ public class SQLiteExecutor implements CustomerDatabaseManager, ManagerDatabaseM
                             rate, base, free, rtype, vtype);
     }
 
-    private double getDistance(Destination destination){
-        Connection connection = null;
-        try {
-            connection = prepareConnection();
-            if (connection != null){
-                String sql = "select distance from distance where province='" + destination.getProvince() + "' and district='" + destination.getDistrict() + "'";
-                Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery(sql);
 
-                if (resultSet.next())
-                    return resultSet.getDouble("distance");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }finally {
-            if (connection != null)
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-        }
-        return 0;
-    }
 
-    private boolean checkPossibleDay(Destination destination, Date startDate, Date endDate){
-        double distance = getDistance(destination);
-        long diff = endDate.getTime() - startDate.getTime();
-        int days = (int) diff / (24*60*60*1000);
-        if (distance/720 > days)
-            return false;
-        return true;
-    }
+
 
     private Connection prepareConnection(){
         try {
--- Server/src/main/java/controllers/delegations/ReservationExecutor.java
+++ Server/src/main/java/controllers/delegations/ReservationExecutor.java
@@ -1,9 +1,35 @@
 package controllers.delegations;
 
+import controllers.assistants.UpdateExecutionAssistant;
+import managers.CustomerDatabaseManager;
+import models.Destination;
+import utils.ReservationDateFormatter;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.logging.SimpleFormatter;
+
 public class ReservationExecutor {
     private String url;
 
     public ReservationExecutor(String url) {
         this.url = url;
+    }
+
+    public void addReservation(String customerId, Map<String, Integer> vanAmt, Destination destination, Date startDate, Date endDate, Date reserveDate, double price, double deposit) {
+        System.out.println("request addReservation");
+        SimpleDateFormat formatter = ReservationDateFormatter.getInstance().getDbFormatter();
+        String reserveDateString = formatter.format(reserveDate);
+        String startDateString = formatter.format(startDate);
+        String endDateString = formatter.format(endDate);
+        String sql = String.format("insert into reservation (customer_id, reserve_date, start_working_date, end_working_date, fee, province, district, place, isDeposit, isDeposited, deposit_fee, amt_vip, amt_normal) values ('%s', '%s', '%s', '%s', %f, '%s', '%s', '%s', '%s', '%s', %f, %d, %d)",
+                customerId, reserveDateString, startDateString, endDateString, price, destination.getProvince(), destination.getDistrict(), destination.getPlace(), "true", "false", deposit, vanAmt.get(CustomerDatabaseManager.VIP), vanAmt.get(CustomerDatabaseManager.NORMAL));
+        UpdateExecutionAssistant assistant = new UpdateExecutionAssistant(url);
+        int result = assistant.execute(sql);
+        System.out.println("result = " + result);
     }
 }
