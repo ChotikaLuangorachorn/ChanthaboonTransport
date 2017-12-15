f16fe467f61a5dd5f0e0f2af357c77b890f4576e
natthapach <natthapach.a@ku.th>
2017-12-15 18:29:47 +0700

    fix artifact

---
+++ Server/src/main/java/FakeMain.java
@@ -0,0 +1,29 @@
+import controllers.SQLiteExecutor;
+import models.PriceFactor;
+import models.Schedule;
+import utils.ReservationDateFormatter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class FakeMain {
+    public static void main(String[] args) throws ParseException {
+//        String s = String.format("hello %s", null);
+//        System.out.println(s);
+//        SQLiteExecutor executor = new SQLiteExecutor();
+////        PriceFactor priceFactor = executor.getPriceFactor();
+////        System.out.println("priceFactor = " + priceFactor);
+//        SimpleDateFormat format = ReservationDateFormatter.getInstance().getDbFormatter();
+////        Schedule schedule = new Schedule("กส-3993", format.parse("2017-11-28 09:05:00"), format.parse("2017-11-28 13:00:00"), "ซ่อมบำรุง", Schedule.JOB);
+//        Schedule schedule = new Schedule("กค-4983", new Date(), new Date(), "1", Schedule.RESERVE);
+//        executor.deleteVanSchedule(schedule);
+        Date date = new Date();
+        System.out.println("date = " + date);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DATE, 18);
+        System.out.println(calendar.getTime());
+    }
+}
